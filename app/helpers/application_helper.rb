0c51d1408ae92ca408f33c9d2eae0417e3d1b5b5
Anthony Underwood <anthony.underwood@hpa.org.uk>
2008-06-17 14:50:41 +0100

    1) changes to css - a start 2) a few fixes to links since in some cases the app may run in a sub_url using passenger(mod_rails) or mongrel with --prefix (there are probably more to fix) 3) had to add a ActionController::AbstractRequest.relative_url_root in the environment.rb to cope with sub_url- should add this to documentation 4) added an edit link for the steps in the task view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def nice_date(date)
+      h date.strftime("%A %d %B %Y - %H:%M %p")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 9) do
+ActiveRecord::Schema.define(:version => 10) do
 
   create_table "open_id_authentication_associations", :force => true do |t|
     t.integer "issued"
@@ -43,10 +43,11 @@ ActiveRecord::Schema.define(:version => 9) do
 
   create_table "tasks", :force => true do |t|
     t.integer  "project_id"
-    t.text     "description"
+    t.text     "short_description"
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_description"
   end
 
   create_table "users", :force => true do |t|
