86cc4688f12f6d079aba845e2748a37b9da6f8bf
Anthony Underwood <anthony.underwood@hpa.org.uk>
2008-06-23 15:06:08 +0100

    task number and step numbers are now relative to the project

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,7 +3,7 @@ class Step < ActiveRecord::Base
   
   def number
     # find the step number within the project not just the step id 
-    task_step_ids = Task.find(self.task).steps.find(:all, :order=>"id").map{|s| s.id}
+    task_step_ids = Task.find(self.task).steps.map{|s| s.id}
     step_number = task_step_ids.index(self.id) + 1
   end
     
