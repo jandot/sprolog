9e3db1fcfef99ce61dc82d9c779eb45b0b44a406
Anthony Underwood <anthony.underwood@hpa.org.uk>
2008-06-25 13:40:42 +0100

    changes to number method to order tasks and steps by id

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ActiveRecord::Base
 
   def number
     # find the task number within the project not just the task id 
-    project_task_ids = Project.find(self.project).tasks.map{|t| t.id}
+    project_task_ids = Project.find(self.project).tasks.find(:all, :order=>"id").map{|t| t.id}
     task_number = project_task_ids.index(self.id) + 1
     
     return 'T' + self.project.id.to_s + '_' + task_number.to_s
