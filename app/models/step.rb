226f752dfd77c6956ab91fb00594f28ff63f71af
Anthony Underwood <anthony.underwood@hpa.org.uk>
2008-08-04 17:12:48 +0100

    Revert "task number and step numbers are now relative to the project"
    This reverts commit f6a061fe5a08f2f48cc365ed8c87e017b872399e.
    
    Conflicts:
    
    	app/models/step.rb
    	app/models/task.rb

--- app/models/step.rb
+++ app/models/step.rb
@@ -6,5 +6,5 @@ class Step < ActiveRecord::Base
     task_step_ids = Task.find(self.task).steps.find(:all, :order=>"id").map{|s| s.id}
     step_number = task_step_ids.index(self.id) + 1
   end
-    
+
 end
